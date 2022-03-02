883daa706030e75993ce9a7b64df026cc6478ec9
fswetnam <95920473+fswetnam@users.noreply.github.com>
2022-03-01 19:14:03 -0500

    commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {Routes, RouterModule } from '@angular/router'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from 'src/home/home.component';
+import { NewComponent } from './new/new.component';
+const Routs: Routes = [
+  {path: 'home', component:HomeComponent},
+  {path: 'new', component:NewComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(Routs)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/new/new.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new',
+  templateUrl: './new.component.html',
+  styleUrls: ['./new.component.css']
+})
+export class NewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  myNew(){
+      window.location.href="patientp"
+  }
+  
+}

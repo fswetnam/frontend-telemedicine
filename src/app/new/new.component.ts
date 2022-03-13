6efea623d16e93dae56b605c5e5d18fc44e4ded0
fswetnam <95920473+fswetnam@users.noreply.github.com>
2022-03-13 16:41:58 -0400

    New Patient Page now adds to DB

--- src/app/new/new.component.ts
+++ src/app/new/new.component.ts
@@ -1,20 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Patient } from '../patient/Patient';
+import { PatientService } from '../patient/patient.service';
 @Component({
   selector: 'app-new',
   templateUrl: './new.component.html',
   styleUrls: ['./new.component.css']
 })
 export class NewComponent implements OnInit {
+  message: any;
 
 
-
-  constructor() { }
+  constructor(private patientService: PatientService) { }
 
   ngOnInit() {
   }
 
-  myNew(){
-      window.location.href="patientp"
+  myNew(form: NgForm){
+    let validPsw = this.checkPassword(form.value.userpassword, form.value.pswRepeat)
+    if(validPsw == true){
+      const response =  this.patientService.savePatient(form.value as Patient).subscribe((data) => {
+        this.message = data;
+        alert("Account Successfully Created!");
+        window.location.href="patientp";
+      });
+      return response;
+    } else {
+      alert("Passwords must match");
+      form.reset();
+      return null;
+    }
+  }
+
+  checkPassword(pass: string, pswRepeat: string): Boolean{
+    if(pass === pswRepeat){
+      return true;
+    } else {
+      return false;
+    }
   }
   
 }

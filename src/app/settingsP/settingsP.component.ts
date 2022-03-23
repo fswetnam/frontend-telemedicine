c8bd55e14f4659de4a0d9977b0a141a353564452
fswetnam <95920473+fswetnam@users.noreply.github.com>
2022-03-23 16:09:40 -0400

    Patient Settings Page

---
+++ src/app/settingsP/settingsP.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settingsP',
+  templateUrl: './settingsP.component.html',
+  styleUrls: ['./settingsP.component.css']
+})
+export class SettingsPComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
